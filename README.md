b8be10eba76e3f00e3f6e0fdb1a2fae93c677c31
jerry <juanino>
2016-12-26 14:10:59 -0500

    inital commit

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # doihavethis
 Compare multi-level directory structure of pictures to some pile of pictures or videos you just found on a disk.
+
+# How it works
+1. git clone the repo
+2. edit master_dir with the top of your photo tree directory
+3. edit questionable_dir with the location of some files in questions to check
+4. run ./doihavethis.py
+5. the output of the script will note "You should preserve" for files in the questionable directory
+   that it can't find matching md5sum's for in the master. (They must not exist or are differnent/corrupt).
+
+# Some issues
+1. I'm lazy. the md5sum code doesn't buffer, so it will use a ridiculous amount of memory if you have a big file
+2. the md5sum of the master and questionable directories are not stored anywhere. it's slow since it calculates them every time. fine by me.
+3. the python dict is in memory for both md5sum (master and questionable) so it can take up a lot of memory
+
+# Why did i do this
+Mostly because I seem to rewrite variations of this all the time and i can't find my code.
+This way I'm documenting it for myself, my friends and any other shmuck who has to combine a bunch of old disks, drives, sd cards, whatever.
+Just copy all your files into a working dir and point the questionable dir at it and your master at the location of all your other photos.
+As drives got bigger, it makes no sense to have bunches of disks laying around.
+
+Also, it is handy if you are not sure which files you pulled off an SD card, or if you happen to (like in my case) blow away or corrupt an SD card
+and use the wonderful photorec and testdisk to recover the files off the chip, but have no idea which files you already downloaded since recovery
+pulls every non-clobbered file off that chip, even if you meant to delete it previously.
---
+++ doihavethis.py
@@ -0,0 +1,82 @@
+#!/usr/bin/python
+
+import os
+import hashlib
+import pprint
+import sys
+
+# Author: Jerry Uanino
+# Purpose: Do I have this file in my picture library?
+
+# Story: During undelete recovery, hard drive recovery, cd/dvd merging
+#        or otherwise combining old drives it is hard to tell
+#        if you already saved some files off a chip.
+#        especially because you might have changed the filename.
+#        
+#        This script is intended to use md5 as a good enough mathematical 
+#        match to make sure you can safely avoid duplicates
+
+# the top level where you store most of your files
+master_dir = "/root/path/to/your/photos"
+
+# directory containing files you just found
+# and have no idea if you already downloaded them
+questionable_dir = "/dir/where/you/copied/files/you/found"
+
+masterlibrary = dict()
+queslibrary= dict()
+
+def write_to_master_dict(checksum,fullpath):
+    # print "Writing", checksum, " as the key for", fullpath
+    masterlibrary[checksum]=fullpath
+
+def write_to_ques_dict(checksum,fullpath):
+    # print "Writing", checksum, " as the key for", fullpath
+    queslibrary[checksum]=fullpath
+
+def dump_library():
+    print("Master library")
+    pprint.pprint(masterlibrary)
+    print("Questionable library")
+    pprint.pprint(queslibrary)
+
+def compare():
+    # start with the questionable dataset as the master
+    for key in queslibrary:
+        ## debug ## print "Looking up key " + key
+	# go to the master to see if it's there
+	if key in masterlibrary.keys():
+	    # print " ---- This file is in the master ----- "
+            #print "Master library file is " + masterlibrary[key]
+            #print "Questionable library file is " + queslibrary[key]
+	    print queslibrary[key] + " is a dup to " + masterlibrary[key]
+        else:
+            print "You should preserve " + queslibrary[key] + " because it is not in the master"
+
+#
+# MAIN
+#
+# walk the master dir 
+for root, dirs, files in os.walk(master_dir):
+    print "root dir",  root
+    for file in files:
+        fullpath = root+"/"+file
+        checksum = hashlib.md5(open(fullpath,'rb').read()).hexdigest()
+        ## debug ## print "hash of file", fullpath, "is ", checksum
+        print ".",
+        sys.stdout.flush() # make sure dots show up one by one
+        # write to dictionary with checksum as key
+        write_to_master_dict(checksum,fullpath)
+    dump_library()
+
+# walk the questionable dir
+for root, dirs, files in os.walk(questionable_dir):
+    print "root dir",  root
+    for file in files:
+        fullpath = root+"/"+file
+        checksum = hashlib.md5(open(fullpath,'rb').read()).hexdigest()
+        print "hash of file", fullpath, "is ", checksum
+        # write to dictionary with checksum as key
+        write_to_ques_dict(checksum,fullpath)
+    dump_library()
+    compare()
