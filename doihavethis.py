f5ddd5d8b5baecc3c8318cf2ea30f417d81446b9
jerry uanino <juanino@gmail.com>
2017-01-15 21:45:56 -0500

    fixed formatting of output and some style in coding

--- doihavethis.py
+++ doihavethis.py
@@ -19,37 +19,43 @@ import sys
 # the top level where you store most of your files
 master_dir = "/root/path/to/your/photos"
 
+
 # directory containing files you just found
 # and have no idea if you already downloaded them
 questionable_dir = "/dir/where/you/copied/files/you/found"
 
 masterlibrary = dict()
-queslibrary= dict()
+queslibrary = dict()
 
-def write_to_master_dict(checksum,fullpath):
+
+def write_to_master_dict(checksum, fullpath):
     # print "Writing", checksum, " as the key for", fullpath
-    masterlibrary[checksum]=fullpath
+    masterlibrary[checksum] = fullpath
+
 
 def write_to_ques_dict(checksum,fullpath):
     # print "Writing", checksum, " as the key for", fullpath
     queslibrary[checksum]=fullpath
 
-def dump_library():
-    print("Master library")
-    pprint.pprint(masterlibrary)
-    print("Questionable library")
-    pprint.pprint(queslibrary)
+
+def dump_library(libraryname, description):
+    num_items = len(libraryname)
+    print "."
+    print "\t\t", description + " has " + str(num_items) + " items in the hash table"
+
 
 def compare():
+    print "\nRunning compare"
+    print "---------------"
     # start with the questionable dataset as the master
     for key in queslibrary:
-        ## debug ## print "Looking up key " + key
-	# go to the master to see if it's there
-	if key in masterlibrary.keys():
-	    # print " ---- This file is in the master ----- "
-            #print "Master library file is " + masterlibrary[key]
-            #print "Questionable library file is " + queslibrary[key]
-	    print queslibrary[key] + " is a dup to " + masterlibrary[key]
+        # debug ## print "Looking up key " + key
+        # go to the master to see if it's there
+        if key in masterlibrary.keys():
+            # print " ---- This file is in the master ----- "
+            # print "Master library file is " + masterlibrary[key]
+            # print "Questionable library file is " + queslibrary[key]
+            print queslibrary[key] + " is a dup to " + masterlibrary[key]
         else:
             print "You should preserve " + queslibrary[key] + " because it is not in the master"
 
@@ -58,25 +64,27 @@ def compare():
 #
 # walk the master dir 
 for root, dirs, files in os.walk(master_dir):
-    print "root dir",  root
+    print "\nroot dir",  root
+    print "computing ", len(files), " chksums"
     for file in files:
         fullpath = root+"/"+file
-        checksum = hashlib.md5(open(fullpath,'rb').read()).hexdigest()
-        ## debug ## print "hash of file", fullpath, "is ", checksum
+        checksum = hashlib.md5(open(fullpath, 'rb').read()).hexdigest()
+        # debug ## print "hash of file", fullpath, "is ", checksum
         print ".",
         sys.stdout.flush() # make sure dots show up one by one
         # write to dictionary with checksum as key
-        write_to_master_dict(checksum,fullpath)
-    dump_library()
+        write_to_master_dict(checksum, fullpath)
+    dump_library(masterlibrary, "master")
 
 # walk the questionable dir
 for root, dirs, files in os.walk(questionable_dir):
     print "root dir",  root
     for file in files:
         fullpath = root+"/"+file
-        checksum = hashlib.md5(open(fullpath,'rb').read()).hexdigest()
+        checksum = hashlib.md5(open(fullpath, 'rb').read()).hexdigest()
         print "hash of file", fullpath, "is ", checksum
         # write to dictionary with checksum as key
-        write_to_ques_dict(checksum,fullpath)
-    dump_library()
-    compare()
+        write_to_ques_dict(checksum, fullpath)
+    dump_library(queslibrary, "questionable")
+
+compare()
