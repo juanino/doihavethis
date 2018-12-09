27ec91e652ed6be567a7bfcd92a78c8b9db4df2b
root <root@ip-172-30-0-177.ec2.internal>
2018-12-09 20:12:59 +0000

    added logging

--- doihavethis.py
+++ doihavethis.py
@@ -5,6 +5,8 @@ import hashlib
 import pprint
 import sys
 import doihavethisconfig as cfg
+import logging
+import time
 
 # Author: Jerry Uanino
 # Purpose: Do I have this file in my picture library?
@@ -17,12 +19,16 @@ import doihavethisconfig as cfg
 #        This script is intended to use md5 as a good enough mathematical 
 #        match to make sure you can safely avoid duplicates
 
+# system tunables
+log_file = cfg.tunables['log_file']
+
 # the top level where you store most of your files
 master_dir = cfg.directories['master_dir']
 
 # directory containing files you just found
 # and have no idea if you already downloaded them
 questionable_dir = cfg.directories['questionable_dir']
+
 
 masterlibrary = dict()
 queslibrary = dict()
@@ -40,8 +46,7 @@ def write_to_ques_dict(checksum,fullpath):
 
 def dump_library(libraryname, description):
     num_items = len(libraryname)
-    print(".")
-    print("\t\t", description + " has " + str(num_items) + " items in the hash table")
+    logger.info("dump_library " + description + " has " + str(num_items) + " items in the hash table")
 
 
 def compare():
@@ -62,29 +67,65 @@ def compare():
 #
 # MAIN
 #
+
+# setup logging
+logger = logging.getLogger('doihavethis-' + __name__)
+logger.setLevel(logging.INFO)
+
+# create a file handler
+handler = logging.FileHandler(log_file)
+handler.setLevel(logging.INFO)
+
+# format for logging
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+
+# add handlers to logger
+logger.addHandler(handler)
+
+logger.info('Starting utility')
+logger.debug('Debug logging must be on')
+
+print("-D- is a directory being scanned and each file is an F")
+print("Starting in 5 seconds")
+time.sleep(5)
+
 # walk the master dir 
 for root, dirs, files in os.walk(master_dir):
-    print("\nroot dir",  root)
-    print("computing ", len(files), " chksums")
+    logger.info("working on the main master library")
+    logger.info("root dir " +  root )
+    logger.info("computing " +  str(len(files)) +  " chksums")
     for file in files:
         fullpath = root+"/"+file
         checksum = hashlib.md5(open(fullpath, 'rb').read()).hexdigest()
-        # debug ## print "hash of file", fullpath, "is ", checksum
-        sys.stdout.write('.')
+        logger.info("hash of file " + fullpath +  " is " +  checksum)
+        sys.stdout.write('F')
         sys.stdout.flush() # make sure dots show up one by one
         # write to dictionary with checksum as key
         write_to_master_dict(checksum, fullpath)
+    logger.info("finished with files in " + root)
     dump_library(masterlibrary, "master")
+    sys.stdout.write('-D-')
+    sys.stdout.flush() # make sure dots show up one by one
+
+sys.stdout.write('\nFinished with master dir and moving onto questionable dir\n')
+sys.stdout.flush() # make sure dots show up one by one
+
 
 # walk the questionable dir
 for root, dirs, files in os.walk(questionable_dir):
-    print("root dir",  root)
+    logger.info("Working on the questionable directory")
+    logger.info("root dir " +  root)
     for file in files:
         fullpath = root+"/"+file
         checksum = hashlib.md5(open(fullpath, 'rb').read()).hexdigest()
-        print("hash of file", fullpath, "is ", checksum)
+        logger.info("hash of file " + fullpath +  " is " + checksum)
+        sys.stdout.write('F')
+        sys.stdout.flush() # make sure dots show up one by one
         # write to dictionary with checksum as key
         write_to_ques_dict(checksum, fullpath)
     dump_library(queslibrary, "questionable")
+    sys.stdout.write('-D-')
+    sys.stdout.flush() # make sure dots show up one by one
 
 compare()
--- sampleconfig.py
+++ sampleconfig.py
@@ -5,4 +5,4 @@ directories = {'master_dir': '/your/main/pics/dir/',
                'questionable_dir': '/top/of/tree/for/your/files/to/check/'
                }
 
-
+tunables = {'log_file': '/root/compare_result.log' }
