fb4bfadefe37aa68fef8a5afe23d5d596ac76ed8
Jerry Uanino <juanino@gmail.com>
2019-01-05 15:31:56 -0500

    added util for getting file scan on windows

---
+++ walk.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+
+# getting a directory listing of all subdirs is kinda annoying on windows
+# this utility helps us just blob the whole tree into a file for grepping somewhere
+
+import os
+import pprint
+import sys
+import time
+from os import path
+
+# track runtime in case we change hash algorithms in future we can compare
+# also when we switch computers this is a good benchmark of I/O for us
+
+start_time = time.time()
+
+# first arg is the pickle to save
+# second arg is the path to walk
+try:
+    dir_to_inventory = sys.argv[1]
+except:
+    print("Supply the directory to scan")
+    sys.exit()
+
+global output_file
+output_file = "output.txt"
+fh = open(output_file,"w")
+
+# walk the directory to inventory
+file = open("output.txt","w")
+for root, dirs, files in os.walk(dir_to_inventory):
+    for file in files:
+        fullpath = root+"/"+file
+        sys.stdout.write('F')
+        sys.stdout.flush() # make sure dots show up one by one
+        fh.write(fullpath + "\n")
+    sys.stdout.write('-D-')
+    sys.stdout.flush() # make sure dots show up one by one
+
+runtime = time.time() - start_time
+fh.close
+print("Time done is -> " + str(runtime) + " seconds.")
+
+
