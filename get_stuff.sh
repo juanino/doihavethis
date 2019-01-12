ab6510fb3716fd1aa68591489025c9495e87dc1b
root <juanino@gmail.com>
2019-01-12 20:33:52 +0000

    added handy util screen to list

--- get_stuff.sh
+++ get_stuff.sh
@@ -1,4 +1,10 @@
 # modify if you are on yum based system
+
+# required stuff
 apt-get install python3-pip
 pip3 install pickledb
 pip3 install filehash
+pip3 install PyYAML
+
+# nice to have
+apt-get install screen
