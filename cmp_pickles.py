ce2510877efb7786b82282f01390bcdbf4011cc0
Jerry Uanino <juanino@gmail.com>
2020-03-01 13:18:58 -0500

    added tqdm progress bar

--- cmp_pickles.py
+++ cmp_pickles.py
@@ -2,6 +2,7 @@
 
 import pickledb
 import sys
+from tqdm import tqdm
 
 # pass 2 files to compare
 try:
@@ -11,34 +12,27 @@ except:
     print("Supply two filenames to compare")
     exit()
 
-# optional third param is to output report blob
-try:
-    report_file = sys.argv[3]
-except:
-    print("no report file passed, that is fine")
-
 db = pickledb.load(left_file,False)
 db2 = pickledb.load(right_file,False)
 
 keys1 = db.getall()
 keys2 = db2.getall()
 
-print("there are " + str(len(keys1)) + " in left file" )
-print("there are " + str(len(keys2)) + " in right file" )
+estimated_keys = len(keys1)
 
 dup_counter = 0
 keep_counter = 0
+pbar = tqdm(total=estimated_keys,desc="Checking",bar_format="{l_bar}{bar}")
 for key in keys1:
+    pbar.update(1)
+    pbar.set_description("Checking ....." + db.get(key)[1:30] + "...." + db.get(key)[-30:])
     if db.exists(key) and db2.exists(key):
-        print("Duplicate " + key + " -> " + str(db.exists(key)) + " - " +  str(db2.exists(key)) + " file left: " + str(db.get(key)) + " file right: " + str(db2.get(key)) )
+        #print("Duplicate " + key + " -> " + str(db.exists(key)) + " - " +  str(db2.exists(key)) + " file left: " + str(db.get(key)) + " file right: " + str(db2.get(key)) )
         dup_counter = dup_counter + 1
     else:
-        print("Preserve  " + key + " -> " + str(db.exists(key)) + " - " +  str(db2.exists(key)) + " file left: " + str(db.get(key)) + " file right: " + str(db2.get(key))  ) 
+        #print("Preserve  " + key + " -> " + str(db.exists(key)) + " - " +  str(db2.exists(key)) + " file left: " + str(db.get(key)) + " file right: " + str(db2.get(key))  ) 
         keep_counter = keep_counter + 1
     
-#for key in keys2:
-   #print(key + " " + db2.get(key))
-
 print("there are " + str(len(keys1)) + " in left file" )
 print("there are " + str(len(keys2)) + " in right file" )
 print("Duplicates (matches) : " + str(dup_counter))
