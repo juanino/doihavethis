1c46c58ed7627628dfd972a24a272ef8c18f4feb
Jerry Uanino <juanino@gmail.com>
2020-02-29 23:20:54 -0500

    removed no longer used programs

--- sampleconfig.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-
-directories = {'master_dir': '/your/main/pics/dir/',
-               'questionable_dir': '/top/of/tree/for/your/files/to/check/'
-               }
-
-tunables = {'log_file': '/root/compare_result.log' }
